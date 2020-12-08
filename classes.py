d444e76b019643e29b2e469b8fe9cdd80a6d4d6a
Ludivine <ludivine.seve@epsi.fr>
2020-12-08 11:11:51 +0100

    fin regles de gestion

--- classes.py
+++ classes.py
@@ -21,19 +21,43 @@ class Bus:
         i = 0
         while self.appel and i <= len(self.listPassager) - 1:
             if type(self.listPassager[i]) is Lyceen:
-                if self.listPassager[i].estAbsent():
+                if self.listPassager[i].estAbsent() or not self.listPassager[i].hasClasse():
                     self.appel = False
             i += 1
 
     def troisClassesMax(self):
-        return 1
+        listeClassePresenteBus = []
+        for passager in self.listPassager:
+            if type(passager) is Lyceen:
+                if passager.getClasse() not in listeClassePresenteBus:
+                    listeClassePresenteBus.append(passager.getClasse())
+        return len(listeClassePresenteBus)<=3
 
-    def unProfReferentEtUnProfPourDixLyceen(self):
-        return 1
+    def unProfReferent(self):
+        profReferentPresent = False
+        for passager in self.listPassager:
+            if type(passager) is Prof:
+                if passager.estReferent():
+                    if profReferentPresent == False:
+                        profReferentPresent = True
+                    else:
+                        return False
+        return profReferentPresent
+
+    def unProfPourDixLyceen(self):
+        nbProf = 0
+        nbEleve = 0
+        for passager in self.listPassager:
+            if type(passager) is Prof:
+                nbProf +=1
+            else : 
+                nbEleve +=1
+        return nbEleve / nbProf <=10
+
 
     def peutPartir(self):
         return self.appelIsOk() and len(
-            self.listPassager) > 0 and self.troisClassesMax() and self.unProfReferentEtUnProfPourDixLyceen()
+            self.listPassager) > 0 and self.troisClassesMax() and self.unProfReferent() and self.unProfPourDixLyceen()
 
     def partir(self, nom):
         print('Le bus %s est parti' % nom)
@@ -68,6 +92,12 @@ class Lyceen(Passager):
             self.absent = True
         return self.absent
 
+    def getClasse(self):
+        return self.nomClasse
+
+    def hasClasse(self):
+        return self.nomClasse != None
+
 
 class Classe:
     def __init__(self, nbMax, nom):
@@ -76,8 +106,9 @@ class Classe:
         self.listLyceen = []
 
     def add_lyceen(self, lyceen):
-        self.listLyceen.append(lyceen)
-        lyceen.setNomClasse(self.nom)
+        if len(self.listLyceen)<self.nbMax:
+            self.listLyceen.append(lyceen)
+            lyceen.setNomClasse(self.nom)
 
     def getNom(self):
         return self.nom
